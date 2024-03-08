5ba89a9634a0b1c0be1a8dadc7152ba078e907c9
SatoX69 <mdjsusbinsus@gmail.com>
2024-03-08 20:49:04 +0600

    update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -5,3 +5,5 @@ for name in names:
   print(name)
   # Code blocks are indented, this prints the items individually
   print(f"I love {name.title()}")
+  
+ print("I love them all") # This is outside of the looping block
