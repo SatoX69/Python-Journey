6c9402c44037063d20de3fc5e22738d9aae0178d
Sato <mdjsusbinsus@gmail.com>
2024-03-08 19:43:09 +0600

    added common.py

---
+++ common.py
@@ -0,0 +1,29 @@
+#print
+name = "Andrew"
+print("Pleasure being acquainted " + name)
+
+
+#f string
+string = f"My friend's name is {name}"
+print(string)
+
+
+#data type(s)
+integer = 10
+string = "10"
+floating_point = 10.00
+#etc
+
+
+#methods 1
+name_with_space = "  jonathan "
+# lstrip, strip, rstrip
+name_with_space = name_with_space.strip()
+print(name_with_space)
+
+
+#mwthods 2
+# upper(), lower(), title(), capitalize()
+name_with_space = name_with_space.title()
+# Black space trimmed out
+print(f"{name_with_space} is my good friend.")
