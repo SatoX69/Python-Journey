be05d7a174c0cc05fa893b97264cf78b38c57dc2
SatoX69 <m12056637@gmail.com>
2024-03-09 17:53:13 +0600

    update dictionary.py

--- practice/dictionary.py
+++ practice/dictionary.py
@@ -33,3 +33,25 @@ for name, fav in array.items(): # Gets both Keys and Its values
     
 for key in array.keys():
   print(key) #Default behavior, keys() isn't needed necessarily
+  
+for key in array.values():
+  print(key) # Opposite of keys(), gets the values from the dict
+  
+
+array = {
+    "Ben": "Beef Wellington",
+    "Anthony": "Sushi",
+    "Sarah": "Baklava",
+    "Johnson": "Chicken",
+    "Johnson": "Lamb",
+    "Johnson": "Cat" # The other Johnsons get omitted
+    }
+    
+for x in set(sorted(array)):
+    print(x)
+    
+set_ = { "Ben", "Jamal", "Ben", "Ben", "Tate", "Walt", "Ben", "Jackson" }
+
+print(set_) # Inline Set
+
+print(array)
