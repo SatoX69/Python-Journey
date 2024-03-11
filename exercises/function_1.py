c506d0a2ae494d4cf82f23c6dcd4eff2bc7b0e48
SatoX69 <mdjsusbinsus@gmail.com>
2024-03-11 19:02:02 +0600

    update function_1.py

--- exercises/function_1.py
+++ exercises/function_1.py
@@ -3,13 +3,17 @@ def compute(n1=None, n2=None, operator=None):
         return "Include Operator:\n\tAdd, Sub, Mul, Div"
     elif not n1 or not n2:
         return "Pass the first two numeric args"
+    
     switch = {
-         'add': n1 + n2,
-         'sub': n1 - n2,
-         'mul': n1 * n2,
-         'div': n1 / n2
-         }
-    return int(switch.get(operator))
+        'add': n1 + n2,
+        'sub': n1 - n2,
+        'mul': n1 * n2,
+        'div': n1 / n2
+    }
+    
+    return switch.get(operator.lower(), "Invalid Op")
 
 result = compute(operator='div', n1=10, n2=2)
 print(result)
+
+# Update, removed int() and added additional return value for invalid operator
