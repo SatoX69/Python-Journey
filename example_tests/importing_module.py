4759cf6929d7088d41cd259034c040ad8dafc7bd
SatoX69 <mdjsusbinsus@gmail.com>
2024-03-12 15:56:04 +0600

    update importing_module.py

--- example_tests/importing_module.py
+++ example_tests/importing_module.py
@@ -1,4 +1,7 @@
 import function_2
+# from function_2 import registrar, code
+# from function_2 import registrar as register
+# from function_2 import * (Imports all functions) and they will get implicitly defined
 
 x = function_2.registrar("Joe", "Biden", field="Clown", occupation="President", age= '> 50')
 print(x)
