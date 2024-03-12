d15e295be1d6280e7ded6b04347fd9b92a98d210
SatoX69 <mdjsusbinsus@gmail.com>
2024-03-12 11:08:45 +0600

    update importing_module.py

--- example_tests/importing_module.py
+++ example_tests/importing_module.py
@@ -1,6 +1,19 @@
 import function_2
 
-x = function_1.registrar("Joe", "Biden", field="Clown", occupation="President", age= '> 50')
+x = function_2.registrar("Joe", "Biden", field="Clown", occupation="President", age= '> 50')
 print(x)
-x = function_1.code(999, "User_10", "User_20", "User_30")
+x = function_2.code(999, "User_10", "User_20", "User_30")
 print(x)
+
+
+"""
+Or we can write:
+import function_2 as process
+
+x = process.registrar("Joe", "Biden", field="Clown", occupation="President", age= '> 50')
+print(x)
+x = process.code(999, "User_10", "User_20", "User_30")
+print(x)
+
+Here the module is imported with the name "process"
+"""
