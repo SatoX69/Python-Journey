c9ac0a767939668e814c262836886c9efd2c1a06
SatoX69 <mdjsusbinsus@gmail.com>
2024-03-12 11:03:26 +0600

    update

--- example_tests/function_2.py
+++ example_tests/function_2.py
@@ -2,15 +2,11 @@ def registrar(first=None, last=None, **userInfo):
     userInfo["first_name"] = first
     userInfo["last_name"] = last
     return userInfo
-    
-register = registrar("Joe", "Biden", field="Clown", occupation="President", age= '> 50')
-print(register)
 
 def code(code, *ref):
     msg = f'Your code: {code}'
     for x in ref:
         msg += f"\nYou have a referral from {x.title()}"
     return msg
-        
-x = code(1001, "User_10", "User_20", "User_30")
-print(x)
+
+# Exported functions must not have any other additional code like in function_1.py
---
+++ example_tests/importing_module.py
@@ -0,0 +1,6 @@
+import function_2
+
+x = function_1.registrar("Joe", "Biden", field="Clown", occupation="President", age= '> 50')
+print(x)
+x = function_1.code(999, "User_10", "User_20", "User_30")
+print(x)
